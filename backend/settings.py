0207b4a776527fd3412035e84b4721aa27c7dad3
Oliver1334 <oliver.taylor95@outlook.com>
2025-09-14 18:26:48 +0100

    Fix cors config

--- backend/settings.py
+++ backend/settings.py
@@ -64,10 +64,33 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",  # Vite dev server
-    # "https://your-frontend-domain.com",  # add when you deploy FE
+# CORS_ALLOWED_ORIGINS = 
+# [
+#     "http://localhost:5173",  # Vite dev server
+#     # "https://your-frontend-domain.com",  # add when you deploy FE
+# ]
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
 ]
 
 ROOT_URLCONF = 'backend.urls'
